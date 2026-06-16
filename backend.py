8c8b922f052c9de24677357b7327d528b822ff6d
Aybenmila <dsa58151@gmail.com>
2026-06-16 21:19:30 +0300

    first commit

---
+++ backend.py
@@ -0,0 +1,302 @@
+import random
+import math
+import time 
+from collections import Counter
+import json
+
+#!Işık ekle
+
+gameData = open("data.json","r",encoding="utf-8").read()
+
+
+
+class Reactions():
+    def __init__(self):
+        self.rules = {
+            (1,2):["NEUTSPWN",3]
+        }
+
+    def count(self,cell:list):
+        cell.sort()
+        sayac = dict(Counter(cell))
+        return sayac
+    
+    def react(self, cell):
+        counts = self.count(cell)
+        results = []
+
+        for (a, b), effect in self.rules.items():
+            if counts.get(a, 0) > 0 and counts.get(b, 0) > 0:
+                results.append(effect)
+
+        return results
+
+
+reactor = Reactions()
+
+class Particle:
+
+    def __init__(self,name:str,particleId:int,pygame,x,y,temp,mass,color,x_velocity=0,y_velocity=0,sh=1,lifeTime = -1):
+        self.name = name
+        self.pygame = pygame
+        self.temp = max(-273.15 , min(temp,9500))
+        self.sh = sh #Oz Isı
+        self.mass = mass
+        self.color = color
+        self.x = x
+        self.y = y
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
+        self.id = particleId
+        self.lifeTime = lifeTime 
+    def draw(self,win):self.pygame.draw.rect(win, self.color, (self.x, self.y, 3, 3))
+    
+    def gravity(self,win,gravity,dt,grid):
+        downCell = grid[min(int(self.y//3)+1,267)][min(int(self.x) //3, 200)]
+        if downCell and any(item.type in ("solid", "powder") or item.type == self.type for item in downCell):
+            self.y_velocity = 0
+            return
+        downPart = [item.type for item in downCell]
+        self.y_velocity += gravity * dt * 15
+        if self.y_velocity >= 3:
+            self.y_velocity = 3
+        self.y += self.y_velocity
+
+
+
+    def checkLifeTime(self):
+        if  self.lifeTime >= 0:    
+            if self.time >= self.lifeTime:
+                return True
+            self.time += 1
+        else:pass
+
+
+
+    def checkCollision(self,grid,x=None,y=None):
+        x = x or self.x
+        y = y or self.y
+        if 0 < x < 800 and 0 < y < 600:
+            cell = grid[int(y)//3][min(int(x) //3, 200)]
+            if len(cell) >= 1 :
+                return [True,(x,y),cell]
+            else:
+                return [False,0]
+        return [False,0]
+
+
+    def move(self,win,dt,grid):pass
+
+
+class Nuclear(Particle):
+    def __init__(self,name:str,particleId:int,pygame,x,y,temp,mass,color,x_velocity=250,y_velocity=250,lifeTime = 600):
+        self.name = name
+        self.pygame = pygame
+        self.temp = temp
+        self.mass = mass
+        self.color = color
+        self.x = x
+        self.y = y
+        self.x_velocity = x_velocity
+        self.y_velocity = y_velocity
+        self.degree = math.radians(random.randint(1,360))
+        self.id = particleId
+        self.lifeTime = lifeTime
+        self.time = 0
+        self.type = "nuclear"
+        super().__init__(name,particleId,pygame,x,y,temp,mass,color,x_velocity,y_velocity,lifeTime=self.lifeTime)
+
+
+    def gravity(self,win,gravity,dt,grid):pass
+
+    def move(self,win,dt,grid):
+        if 0 < self.x < 800 and 0 < self.y < 600:
+            cell = grid[min(int(self.y//3),267)][min(int(self.x) //3, 200)]
+            self.x += math.cos(self.degree) * self.x_velocity * dt * 0.5
+            self.y += math.sin(self.degree) * self.y_velocity * dt * 0.5
+            if self not in cell:
+                cell.append(self)
+                return cell
+
+
+
+
+
+class Game():
+    def __init__(self,gravity,tick):
+        self.gravity = gravity
+        self.tick = tick
+        self.veri = json.loads(gameData)
+    
+    def reaction(self,cell):
+        particles = [int(particle.id) for particle in cell]
+        result = ["0",cell]
+        if reactor.react(particles):
+            result = [reactor.react(particles),cell]
+        return result
+
+
+    
+    def spawner(self,id,x,y,pygame):
+        tip = self.veri["particles"][str(id)]["type"]
+        particle = self.veri["particles"][str(id)]
+        match tip :
+            
+            case "nuclear":
+                part = Nuclear(particle["name"],id,pygame,x,y,int(particle["temp"]),int(particle["mass"]),tuple(particle["color"]))
+                return part
+
+            case "powder":
+                part = Powder(particle["name"],id,pygame,x,y,int(particle["temp"]),int(particle["mass"]),tuple(particle["color"]))
+                return part
+
+            case "solid":
+                part = Solid(particle["name"],id,pygame,x,y,int(particle["temp"]),int(particle["mass"]),tuple(particle["color"]))
+                return part
+
+            case "liquid":
+                part = Liquid(particle["name"],id,pygame,x,y,int(particle["temp"]),int(particle["mass"]),tuple(particle["color"]))
+                return part
+
+
+            case _:
+                pass
+
+    # def heat(self, cell) -> None:
+    #     ust = 0
+    #     alt = 0
+
+    #     for part in cell:
+    #         ust += part.temp * part.sh * part.mass
+    #         alt += part.mass * part.sh
+
+    #     result = ust / alt if alt > 0 else -274
+
+    #     if 9500 >  result > -274 :
+    #         for part in cell:
+    #             heat_capacity = part.mass * part.sh
+
+    #             # İletkenlik/hız katsayısı
+    #             transfer_rate = 0.1
+
+    #             # Denge sıcaklığına doğru yaklaş
+    #             delta = (result - part.temp) * transfer_rate
+
+    #             # Öz ısısı ve kütlesi büyük olanlar daha yavaş değişsin
+    #             part.temp += delta / max(heat_capacity, 1e-6)
+
+
+
+    def elementData(self,id):
+        return self.veri["particles"][str(id)]
+
+
+class Powder(Particle):
+    def __init__(self,name:str,particleId:int,pygame,x,y,temp,mass,color):
+        self.name = name
+        self.particleID = particleId
+        self.pygame = pygame
+        self.x = x 
+        self.y = y
+        self.temp = temp
+        self.mass = mass
+        self.color = color
+        self.type = "powder"
+
+        super().__init__(name,particleId,pygame,x,y,temp,mass,color)
+
+
+
+    def move(self,win,dt,grid):
+        downCell = []
+        moveTo = random.randint(0,1) * 2 - 1
+        if 0 < self.x < 800 and 0 < self.y < 600:
+            downCell = grid[min(int(self.y//3)+1,267)][min(int(self.x) //3, 200)]
+            if len(downCell) > 0:
+
+                gotoCell = grid[min(int(self.y//3+1),267)][min(int(self.x) //3+moveTo, 200)]
+                if len(gotoCell) > 0 and any(item.type in ("solid", "powder") or item.type == self.type for item in gotoCell):
+                    moveTo *= -1
+                gotoCell = grid[min(int(self.y//3+1),267)][min(int(self.x) //3+moveTo, 200)]
+                if len(gotoCell) > 0 and any(item.type in ("solid", "powder") or item.type == self.type for item in gotoCell):
+                    moveTo *= 0
+            else:moveTo *= 0
+
+
+            self.x += moveTo
+            cell = grid[min(int(self.y//3),267)][min(int(self.x+moveTo) //3, 200)]
+            
+            if self not in cell:
+                cell.append(self)
+                return cell
+
+
+class Solid(Particle):
+    def __init__(self,name:str,particleId:int,pygame,x,y,temp,mass,color):
+        self.name = name
+        self.particleID = particleId
+        self.pygame = pygame
+        self.x = x 
+        self.y = y
+        self.temp = temp
+        self.mass = mass
+        self.color = color
+        self.type = "solid"
+
+        super().__init__(name,particleId,pygame,x,y,temp,mass,color)
+
+
+    def gravity(self,win,gravity,dt,grid):pass
+
+    def move(self,win,dt,grid):
+        grid[min(int(self.y//3),267)][min(int(self.x) //3, 200)].append(self)
+
+
+
+class Liquid(Particle):
+    def __init__(self,name:str,particleId:int,pygame,x,y,temp,mass,color):
+        self.name = name
+        self.particleID = particleId
+        self.pygame = pygame
+        self.x = x 
+        self.y = y
+        self.temp = temp
+        self.mass = mass
+        self.color = color
+        self.type = "liquid"
+
+        super().__init__(name,particleId,pygame,x,y,temp,mass,color)
+
+
+
+    def move(self,win,dt,grid):
+        downCell = []
+        moveTo = random.randint(0,1) * 2 - 1
+        if 0 < self.x < 800 and 0 < self.y < 600:
+            upCell = grid[min(int(self.y//3)-1,267)][min(int(self.x) //3, 200)]
+            downCell = grid[min(int(self.y//3)+1,267)][min(int(self.x) //3, 200)]
+            if not len(upCell) > 0 and len(downCell) > 0:
+
+                gotoCell = grid[min(int(self.y//3),267)][min(int(self.x) //3+moveTo, 200)]
+                if len(gotoCell) > 0:
+                    moveTo *= -1
+                gotoCell = grid[min(int(self.y//3),267)][min(int(self.x) //3+moveTo, 200)]
+                if len(gotoCell) > 0:
+                    moveTo *= 0
+
+
+            self.x += moveTo
+            cell = grid[min(int(self.y//3),267)][min(int(self.x) //3, 200)]
+            if any(item.type == "powder" for item in cell):
+                self.y -= 50
+                cell = grid[min(int(self.y//3)-3,267)][min(int(self.x) //3, 200)]
+                
+            if self not in cell:
+                cell.append(self)
+                return cell
+
+
+
+
+
+#! Işık ekle
---
+++ main.py
@@ -0,0 +1,151 @@
+import pygame
+import backend
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 600
+
+
+
+#!ViewMode Ekle
+#!Sıvı ve  Gaz ekle
+#!Daha fazla element ekle
+#!Sıcaklık ve ısı sistemini kur
+#!Json ile tepkime datalarını tut
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Sandbox")
+
+running = True
+parts = []
+font = pygame.font.SysFont("Arial", 24)
+
+#!Stres testi
+# for _ in range(4000):
+#     proton = backend.Nuclear("proton",2,pygame,200,150,24,1,(255, 0, 0))
+#     electron = backend.Nuclear("electron",1,pygame,200,450,24,1,(255, 255, 255))
+
+#     parts.append(proton)
+#     parts.append(electron)
+
+#* Tek particle testi
+# electron = backend.Nuclear(pygame,267,200,24,1,(255, 255, 255))
+# parts.append(electron)
+
+
+
+clock = pygame.time.Clock()
+game = backend.Game(9.98,60)
+delete = []
+selected = 1
+
+
+
+
+
+while running:
+    grid = [[[] for _ in range(-5,267)] for _ in range(-5,200)]
+    saat = clock.tick(game.tick)
+    DT = saat / 1000
+    win.fill((0,0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_TAB:
+                selected += 1 
+
+                if selected > 6:
+                    selected = 1 
+
+    if pygame.mouse.get_pressed()[0]:
+        x, y = pygame.mouse.get_pos()
+
+        occupied = False
+
+        for part in parts:
+            if int(part.x // 3) == int(x // 3) and int(part.y // 3) == int(y // 3):
+                occupied = True
+                break
+
+        if not occupied:
+            part = game.spawner(selected, x, y, pygame)
+            parts.append(part)
+            
+
+
+        
+    
+    
+    
+    
+    
+    for part in parts:
+        part.gravity(win,game.gravity,DT,grid)
+        part.move(win,DT,grid)
+        part.draw(win)
+        
+        if 0 > part.x or part.x >800:
+            try:
+                delete.append(part)
+            except:pass
+
+        if 0 > part.y or part.y >600:
+            try:
+                delete.append(part)
+            except:pass
+    
+
+        if part.checkLifeTime():
+            delete.append(part)
+
+
+        result = part.checkCollision(grid)
+        if result[0]:
+            synthesis = []
+            data = game.reaction(result[2])
+            # heat = game.heat(result[2])
+            match data[0][0][0]:
+                case "NEUTSPWN":
+                    part = game.spawner(data[0][0][1],result[1][0],result[1][1],pygame)
+                    parts.append(part)
+                    synthesis.append(part)
+                    delete = [x for x in data[1] if x not in synthesis]
+
+
+
+
+
+
+                case "0":pass
+                case _:pass
+                
+        # if part.type == "powder":
+        #     print(part.y_velocity)
+
+    if pygame.mouse.get_pressed()[2]:
+        delete.extend(grid[max(0, min(pygame.mouse.get_pos()[1]//3, 200))][max(0, min(pygame.mouse.get_pos()[0]//3, 267))])
+
+    for part in delete:
+        try:parts.remove(part)
+        except:pass
+
+    delete = []
+
+
+
+
+
+    
+    win.blit(font.render(f"Particle:{len(parts)}",True,(255,255,255)),(0,0))
+    win.blit(font.render(f"selected particle:{game.elementData(selected)["name"]}",True,(255,255,255)),(0,20))
+    pygame.display.flip()
+
+
+    
+
+pygame.quit()
